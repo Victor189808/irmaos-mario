4049d1b45797d7fb7c75262e44899ddbcae3d1df
victor02022020 <Victor189808@gmail.com>
2024-02-16 23:54:50 -0300

    atualização 16/02

--- scripts.js
+++ scripts.js
@@ -14,3 +14,25 @@ function esconderForm(){
     mascara.style.visibility = "hidden" 
 
 }
+const texto = [
+    "Fazemos reparos nos seu encanamentos!",
+    "Arrumamos torneiras!",
+    "Canos furados!",
+    "Seu vaso entupiu? Chame os irmãos Mario!",
+    "Somos a solução dos seus problemas!"
+];
+
+const divTexto = document.getElementById('texto');
+let index = 0;
+
+function digitarTexto() {
+    divTexto.textContent = frases[index];
+    divTexto.style.animation = 'typing 5s steps(40, end)';
+    index = (index + 1) % frases.length;
+}
+
+// Chama a função para iniciar a digitação do primeiro texto
+digitarTexto();
+
+// Configura um intervalo para digitar os textos subsequentes
+setInterval(digitarTexto, 6000); // Mude o tempo conforme desejado (em milissegundos)
